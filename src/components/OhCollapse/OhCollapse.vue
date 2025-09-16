1e9050dd4ff5c7f939c2b9ae8a5b519c28730673
Soi <ycrxun@163.com>
2025-09-16 20:53:18 +0800

    Add OhCollapse component

---
+++ src/components/OhCollapse/OhCollapse.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="oh-collapse">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps({});
+</script>
+
+<style scoped>
+.oh-collapse {
+  @apply border rounded;
+}
+</style>
