000c03bad1af4afbcf44232e0d15f1539497a1ff
Soi <ycrxun@163.com>
2025-09-16 20:53:34 +0800

    Add OhGrid component

---
+++ src/components/OhGrid/OhGrid.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="oh-grid">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps({});
+</script>
+
+<style scoped>
+.oh-grid {
+  @apply grid grid-cols-2 gap-4;
+}
+</style>
