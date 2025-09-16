132ecf625d6c9d748411cad8a563409bbc4f1b57
Soi <ycrxun@163.com>
2025-09-16 20:52:54 +0800

    Add OhNavbar component

---
+++ src/components/OhNavbar/OhNavbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav class="oh-navbar">
+    <slot name="left" />
+    <h1 class="oh-navbar__title"><slot /></h1>
+    <slot name="right" />
+  </nav>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps({
+  title: {
+    type: String,
+    default: '',
+  },
+});
+</script>
+
+<style scoped>
+.oh-navbar {
+  @apply flex items-center justify-between px-4 py-2 bg-gray-800 text-white;
+}
+.oh-navbar__title {
+  @apply text-lg font-bold;
+}
+</style>
