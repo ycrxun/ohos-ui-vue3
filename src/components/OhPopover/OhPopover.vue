5aff5277aa23b57ff7f5c40345914fc3a4c16928
Soi <ycrxun@163.com>
2025-09-16 20:53:41 +0800

    Add OhPopover component

---
+++ src/components/OhPopover/OhPopover.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="oh-popover">
+    <slot name="trigger" />
+    <div v-if="visible" class="oh-popover__content">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps({
+  visible: {
+    type: Boolean,
+    default: false,
+  },
+});
+</script>
+
+<style scoped>
+.oh-popover {
+  @apply relative;
+}
+.oh-popover__content {
+  @apply absolute bg-white border p-2 shadow rounded;
+}
+</style>
