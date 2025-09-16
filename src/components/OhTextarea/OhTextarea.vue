c456dc3bd4c9a6214b39f1de8f017a34c7f403bf
Soi <ycrxun@163.com>
2025-09-16 20:54:09 +0800

    Add OhTextarea component

---
+++ src/components/OhTextarea/OhTextarea.vue
@@ -0,0 +1,23 @@
+<template>
+  <textarea
+    class="oh-textarea"
+    v-bind="$attrs"
+    :value="modelValue"
+    @input="$emit('update:modelValue', $event.target.value)"
+  />
+</template>
+
+<script lang="ts" setup>
+const props = defineProps({
+  modelValue: {
+    type: String,
+    default: '',
+  },
+});
+</script>
+
+<style scoped>
+.oh-textarea {
+  @apply border rounded px-4 py-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-500 resize-none;
+}
+</style>
