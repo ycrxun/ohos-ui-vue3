cf810e8a1929ef7cd62a964fe0ca8adb4236180f
Soi <ycrxun@163.com>
2025-09-16 20:55:14 +0800

    Add OhTag component

---
+++ src/components/OhTag/OhTag.vue
@@ -0,0 +1,32 @@
+<template>
+  <span class="oh-tag" :class="`oh-tag--${type}`">
+    <slot />
+  </span>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps({
+  type: {
+    type: String,
+    default: 'default',
+  },
+});
+</script>
+
+<style scoped>
+.oh-tag {
+  @apply px-2 py-1 text-sm rounded font-medium;
+}
+.oh-tag--default {
+  @apply bg-gray-500 text-white;
+}
+.oh-tag--success {
+  @apply bg-green-500 text-white;
+}
+.oh-tag--warning {
+  @apply bg-yellow-500 text-white;
+}
+.oh-tag--error {
+  @apply bg-red-500 text-white;
+}
+</style>
