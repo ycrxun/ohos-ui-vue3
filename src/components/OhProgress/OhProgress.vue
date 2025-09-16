fe41a2d12a0fbcff56d3088eaf661f30afaa0602
Soi <ycrxun@163.com>
2025-09-16 20:55:44 +0800

    Add OhProgress component

---
+++ src/components/OhProgress/OhProgress.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="oh-progress">
+    <div class="oh-progress__bar" :style="{ width: progress + '%' }"></div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps({
+  progress: {
+    type: Number,
+    required: true,
+  },
+});
+</script>
+
+<style scoped>
+.oh-progress {
+  @apply w-full h-4 bg-gray-200 rounded overflow-hidden;
+}
+.oh-progress__bar {
+  @apply h-full bg-blue-500 transition-all;
+}
+</style>
