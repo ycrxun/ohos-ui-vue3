3ffd432e8dd0aa240ec10720cc975d827f50d280
Soi <ycrxun@163.com>
2025-09-16 20:53:08 +0800

    Add OhTabBar component

---
+++ src/components/OhTabBar/OhTabBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="oh-tabbar">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps({});
+</script>
+
+<style scoped>
+.oh-tabbar {
+  @apply fixed bottom-0 w-full bg-gray-800 text-white flex justify-around py-2;
+}
+</style>
