365995ab3e254afec6cc1089dedce81ea1c83cf9
Soi <ycrxun@163.com>
2025-09-16 20:54:44 +0800

    Add OhToast component

---
+++ src/components/OhToast/OhToast.vue
@@ -0,0 +1,20 @@
+<template>
+  <div v-if="visible" class="oh-toast">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps({
+  visible: {
+    type: Boolean,
+    default: false,
+  },
+});
+</script>
+
+<style scoped>
+.oh-toast {
+  @apply fixed bottom-4 right-4 bg-gray-800 text-white px-4 py-2 rounded shadow;
+}
+</style>
